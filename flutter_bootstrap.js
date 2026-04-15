9b482ba5b1ef72979e1ac1d1ba549771743705f2
jawad427 <jawad.satussystems@outlook.com>
2026-04-16 01:48:24 +0500

    feat(Ui): Ui modified for the portfolio

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a6bd3f1de158bb61090e0c8053df93a10cb548
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1280924999"
+    serviceWorkerVersion: "3701829503"
   }
 });
